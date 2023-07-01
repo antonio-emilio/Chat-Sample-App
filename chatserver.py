3c13cf3deaa4ebfb8b5b13a805318db56b7c7a34
Antonio <antonio_pereira@outlook.com>
2023-07-01 14:25:20 -0300

    feat: working on chat

--- chatserver.py
+++ chatserver.py
@@ -1,48 +1,42 @@
-from socket  import *
+from socket import *
 import pickle
-import const #- addresses, port numbers etc. (a rudimentary way to replace a proper naming service)
+import const
+import threading
 
-server_sock = socket(AF_INET, SOCK_STREAM) # socket for clients to connect to this server
+server_sock = socket(AF_INET, SOCK_STREAM)
 server_sock.bind(('0.0.0.0', const.CHAT_SERVER_PORT))
-server_sock.listen(5) # may change if too many clients
+server_sock.listen(5)
 
 print("Chat Server is ready...")
 
-while True:
-    #
-    # Get a message from a sender client
-    (conn, addr) = server_sock.accept()  # returns new socket and addr. client
-    
-    #print("Chat Server: client is connected from address " + str(addr))
-
-    marshaled_msg_pack = conn.recv(1024)   # receive data from client
+def handle_client_connection(conn, addr):
+    marshaled_msg_pack = conn.recv(1024)
     msg_pack = pickle.loads(marshaled_msg_pack)
     msg = msg_pack[0]
     dest = msg_pack[1]
     src = msg_pack[2]
-    print("RELAYING MSG: " + msg + " - FROM: " + src + " - TO: " + dest) # just print the message and destination
-    #
-    # Check that the destination exists
+    print("RELAYING MSG: " + msg + " - FROM: " + src + " - TO: " + dest)
+
     try:
-        dest_addr = const.registry[dest] # get address of destination in the registry
+        dest_addr = const.registry[dest]
     except:
-        conn.send(pickle.dumps("NACK")) # to do: send a proper error code
-        continue
-    else:
-        #print("Server: sending Ack to " + src)
-        conn.send(pickle.dumps("ACK")) # send ACK to client
-    conn.close() # close the connection
-    #
-    # Forward the message to the recipient client
-    client_sock = socket(AF_INET, SOCK_STREAM) # socket to connect to clients
+        conn.send(pickle.dumps("NACK"))
+        conn.close()
+        return
+
+    conn.send(pickle.dumps("ACK"))
+    conn.close()
+
+    client_sock = socket(AF_INET, SOCK_STREAM)
     dest_ip = dest_addr[0]
     dest_port = dest_addr[1]
     try:
-        #print("Server: Trying to connect to (" + dest_ip + "," + str(dest_port) + ")")
         client_sock.connect((dest_ip, dest_port))
     except:
-        print ("Error: Destination client is down")
-        continue
+        print("Error: Destination client is down")
+        client_sock.close()
+        return
+
     msg_pack = (msg, src)
     marshaled_msg_pack = pickle.dumps(msg_pack)
     client_sock.send(marshaled_msg_pack)
@@ -50,10 +44,13 @@ while True:
     reply = pickle.loads(marshaled_reply)
     if reply != "ACK":
         print("Error: Destination client did not receive message properly")
-    else:
-        #print("Server: Received Ack from client")
-        pass
     client_sock.close()
 
+def start_server():
+    while True:
+        (conn, addr) = server_sock.accept()
+        client_thread = threading.Thread(target=handle_client_connection, args=(conn, addr))
+        client_thread.start()
+        print("Client connected to server at " + str(addr) + " - Starting thread for client")
 
-
+start_server()
--- const.py
+++ const.py
@@ -1,5 +1,5 @@
-CHAT_SERVER_HOST = "10.128.0.3"
+CHAT_SERVER_HOST = "127.0.0.1"
 CHAT_SERVER_PORT = 5001
 
 # IDs de usuários registrados com os respectivos endereços IP e números de porta
-registry = {"Alice":("10.128.0.2",5002), "Bob":("10.128.0.4",5002)}
+registry = {"Alice":("127.0.0.1",5002), "Bob":("127.0.0.1",5002)}
